38fdb3b6b4c3bcf3804d037f6b9973adc0b8da97
Stefan <stefanbentz@gmx.de>
2025-03-07 10:59:21 +0100

    even more cats

--- kardinalitaet_1_1/03_1-1.sql
+++ kardinalitaet_1_1/03_1-1.sql
@@ -1,33 +1,63 @@
 -- Vorbereitungen
+DROP TABLE IF EXISTS design.servants;
+DROP TABLE IF EXISTS design.cats;
 
 
 
 -- Mastertabelle (MT): unverändert
-
+CREATE TABLE IF NOT EXISTS design.cats
+(
+  id         INT         NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  catname    VARCHAR(45) NOT NULL,
+  fur colour VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+) COMMENT 'Tabs mit Katzen';
 
 -- Struktur: MT
-
+DESCRIBE design.cats;
 
 -- Inserts: MT 
+INSERT INTO design.cats (id, cat_name,fur_color) VALUES 
+(DEFAULT, "Grizabella", "white"),
+(DEFAULT, "Alonzo", "grey"),
+(DEFAULT, "Mausi", "striped")
+;
 
 
 -- Inhalte: MT
-
+SELECT * FROM design.cats;
 
 
 -- Detailtabelle: Verbindung zur MT über Fremdschlüssel
-
+CREATE TABLE IF NOT EXISTS design.servants
+(
+  id           INT         NOT NULL AUTO_INCREMENT COMMENT 'PK',
+  servant_name VARCHAR(45) NOT NULL,
+  yrs_served   TINYINT     NULL    ,
+  cats_id      INT         NOT NULL COMMENT 'FK',
+  PRIMARY KEY (id)
+);
 
 -- Fremdschlüssel: DT
-
+ALTER TABLE design.servants
+  ADD CONSTRAINT FK_cats_TO_servants
+    FOREIGN KEY (cats_id)
+    REFERENCES cats (id);
 
 -- wichtig bei 1:1  UNIQUE im fk
-
+ALTER TABLE design.servants
+  ADD CONSTRAINT UQ_cats_id UNIQUE (cats_id);
 
 -- Struktur: DT
-
+DESCRIBE design.servants;
 
 -- Inserts: DT
+INSERT INTO design.servants (id, servant_name, yrs_served, cats_id) VALUES 
+(DEFAULT, "Max", 5, 1),
+(DEFAULT, "Mirko", 2, 2),
+(DEFAULT, "Stefan", 10, 3)
+;
 
 
 -- Inhalte: DT
+SELECT * FROM design.servants;
